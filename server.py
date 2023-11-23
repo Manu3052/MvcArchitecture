e60a00ea591bb11b5c373674c274287edd3ab243
Manuele Maurício <100435021+Manu3052@users.noreply.github.com>
2023-11-23 10:43:31 -0300

    feat: criando server

---
+++ server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.routers import users
+
+app = FastAPI()
+
+# Cors settings
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
+app.include_router(users.router, tags=["Users"])
