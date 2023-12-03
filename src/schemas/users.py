8a2b9f345de7342cf14ae91914ca972f1222a59d
Manuele Maurício <100435021+Manu3052@users.noreply.github.com>
2023-12-03 12:12:14 -0300

    feat: criando schema UserLogin

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class CreateUserSchema(BaseModel):
@@ -8,10 +8,12 @@ class CreateUserSchema(BaseModel):
     Attributes:
         name (str): This camp is a string which represents the name
         password (str): This camp is a string which represents the password
+        email (EmailStr): This camp is the user email address
     """
 
     name: str
     password: str
+    email: EmailStr
 
 
 class UserSchema(BaseModel):
@@ -21,12 +23,12 @@ class UserSchema(BaseModel):
     Attributes:
         id (int): This camp is an int which represents the id
         name (str): This camp is a string which represents the name
-        password (str): This camp is a string which represents the password
+        email (str): This camps is a string which the email
     """
 
     id: int
     name: str
-    password: str
+    email: str
 
 
 class UserUpdateSchema(BaseModel):
@@ -37,4 +39,17 @@ class UserUpdateSchema(BaseModel):
         password (str): This camp is a string which represents the password
     """
 
+    password: str
+
+
+class UserLogin(BaseModel):
+    """
+    This class is a schema used for the user login
+
+    Attributes:
+        email (str): this camp is a string which represents the email
+        password (str): this camp is a password which represents the password
+    """
+
+    email: str
     password: str
