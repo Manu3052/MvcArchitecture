7f1eb5413b5d953f18f495bfa5844227d3f4ee6f
Manuele Maurício <100435021+Manu3052@users.noreply.github.com>
2023-12-04 21:47:08 -0300

    feat: cadastrando url de auth

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.routers import users
+from src.routers import auth, users
 
 app = FastAPI()
 
@@ -21,3 +21,4 @@ app.add_middleware(
 
 # Routes
 app.include_router(users.router, tags=["Users"])
+app.include_router(auth.router, tags=["Auth"])
